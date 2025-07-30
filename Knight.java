1087072149a62a13ff5fd7ebe677ed33a8d0a265
Aidan <sanchezar006@gmail.com>
2025-07-30 13:16:58 -0700

    main

--- Knight.java
+++ Knight.java
@@ -1,4 +1,6 @@
 public class Knight extends GameMethods {
+    Scanner input = new Scanner(System.in);
+
     public static void runKnightStory() {
         System.out.println("Running Knight pathway."); 
         
@@ -7,5 +9,15 @@ public class Knight extends GameMethods {
         You have to go to the old castle of Poachers Peak and slay the dragon in order to return to Roven Telia. 
         You were geared with your basic standard issue gear and some potions by some mage and sent into the forest.");
 
+    System.out.println("");
+
+    System.out.println("While strolling through the deadly forest you were ambushed by a group of Goblins. 
+        You didn’t speak their tongue but you could tell they wanted everything you had on you.");
+
+    System.out.println("");
+
+    System.out.println("+------------------------+");
+    System.out.println("|         Fight!         |");
+    System.out.println("+------------------------+");    
     }
 }
