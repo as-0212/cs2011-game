d1305eef5adb41e22bff6febadebbe93ad281d2b
Aidan <sanchezar006@gmail.com>
2025-07-30 13:08:04 -0700

    main

--- Knight.java
+++ Knight.java
@@ -1,7 +1,11 @@
 public class Knight extends GameMethods {
     public static void runKnightStory() {
-        System.out.println("Running Knight pathway."); //Placeholder
-        //This would essentially be the "main" for the Knight path.
-        //Any scripting, dialouge, fights, etc. would go here instead of Game.
+        System.out.println("Running Knight pathway."); 
+        
+    System.out.println("You were once a well respected knight in the kingdom of Roven Telia, 
+        but after some controversy about you and the princess, you were banished and sent on a suicide mission. 
+        You have to go to the old castle of Poachers Peak and slay the dragon in order to return to Roven Telia. 
+        You were geared with your basic standard issue gear and some potions by some mage and sent into the forest.");
+
     }
 }
